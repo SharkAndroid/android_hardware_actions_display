1cddf7516c75430e36d45e3a7259ee7be30ffc57
iBullRay <bullray097@yandex.ru>
2015-12-08 14:45:05 +0200

    display: Fixed error with syntax
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Display driver for ATM702X
-ifeq ($(BOARD_HAVE_ACTIONS_DISPLAY), true)
+ifeq ($(strip $(BOARD_HAVE_ACTIONS_DISPLAY)), true)
 
 LOCAL_PATH := $(call my-dir)
 
